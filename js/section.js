55585a8ac64c951cf58e5f4b4979c02d0fb2512e
alaa <you@example.com>
2020-03-20 19:05:26 +0200

    my web

---
+++ js/section.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("a[href='']").mouseenter(function(){
+		$(this).css("color","#EF476F");
+	});
+	$("a[href='']").mouseleave(function(){
+		$(this).css("color","white");
+	});
+});
+
+
